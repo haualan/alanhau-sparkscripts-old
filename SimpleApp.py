8ea0ce468ee37483f47eeb692549f0531db7eb31
haualan <alan.wc.hau@gmail.com>
2015-03-06 00:17:28 -0500

    ..

--- SimpleApp.py
+++ SimpleApp.py
@@ -1,8 +1,10 @@
 """SimpleApp.py"""
 from pyspark import SparkContext
+# this is the location of the cluster
+master = "spark://ec2-52-10-98-37.us-west-2.compute.amazonaws.com:7077"
 
 logFile = "googlebooks-eng-all-1gram-20120701-other"  # Should be some file on your system
-sc = SparkContext("local", "Simple App")
+sc = SparkContext(master, "Simple App")
 logData = sc.textFile(logFile).cache()
 
 numAs = logData.filter(lambda s: 'a' in s).count()
